b38c1aa196f6373b7e4cadd40d95851f2b4db223
Sujay Jeppu <sujayrj@gmail.com>
2024-05-17 19:27:37 +0530

    added Exception handling for Product Controller API

---
+++ catalog-service/src/main/java/dev/jeppu/bookstore/catalog/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package dev.jeppu.bookstore.catalog.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final URI NOT_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/not-found");
+    private static final URI ISE_TYPE = URI.create("https://api.bookstore.com/errors/server-error");
+    private static final String SERVICE_NAME = "Catalog Service";
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ProblemDetail handleProductNotFoundException(ProductNotFoundException productNotFoundException) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, productNotFoundException.getMessage());
+        problemDetail.setTitle("Product Not Found");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service_name", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(Exception exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setType(ISE_TYPE);
+        problemDetail.setProperty("service_name", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+}
---
+++ catalog-service/src/main/java/dev/jeppu/bookstore/catalog/web/exception/ProductNotFoundException.java
@@ -0,0 +1,11 @@
+package dev.jeppu.bookstore.catalog.web.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+
+    public static ProductNotFoundException forCode(String code) {
+        return new ProductNotFoundException("Product not found for code: " + code);
+    }
+}
